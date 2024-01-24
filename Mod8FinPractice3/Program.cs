1389e9dc51e387a56012175e67db946cf18057e7
JJibbz <vkazancev322@gmail.com>
2024-01-24 18:15:31 +0700

    Добавьте файлы проекта.

---
+++ Mod8FinPractice3/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Mod8FinPractice3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\lino-\OneDrive\Рабочий стол\Новая папка (3)";
+            Console.WriteLine($"Исходный размер папки:{GetFolderSize(path)}");
+            long memoryBeforeCollection = GetFolderSize(path);
+            DeleteUnusedFiles(path);
+            long releasedMemory = memoryBeforeCollection - GetFolderSize(path);
+            Console.WriteLine($"Освобождено: {releasedMemory}");
+            Console.WriteLine($"Текущий размер папки:{GetFolderSize(path)}");
+        }
+
+        static long GetFolderSize(string path)
+        {
+            try
+            {
+                long size = 0;
+                if (Directory.Exists(path))
+                {
+                    DirectoryInfo directory = new DirectoryInfo(path);
+                    FileInfo[] files = directory.GetFiles();
+                    foreach (FileInfo file in files)
+                    {
+                        size += file.Length;
+                    }
+                    DirectoryInfo[] folders = directory.GetDirectories();
+                    foreach (DirectoryInfo folder in folders)
+                    {
+                        size += GetFolderSize(folder.FullName);
+                    }
+                    return size;
+                }
+                else
+                {
+                    Console.WriteLine("Каталога не существует либо введён неверный путь");
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        static void DeleteUnusedFiles(string path)
+        {
+            try
+            {
+                DateTime currentTime = DateTime.Now;
+                DateTime lifeSpan = currentTime.AddMinutes(-30);
+                if (Directory.Exists(path))
+                {
+                    DirectoryInfo directory = new DirectoryInfo(path);
+                    DirectoryInfo[] folders = directory.GetDirectories();
+                    foreach (DirectoryInfo folder in folders)
+                    {
+                        DateTime lastAccess = Directory.GetLastAccessTime(folder.FullName);
+                        if (lastAccess <= lifeSpan)
+                        {
+                            DeleteUnusedFiles(folder.FullName);
+                            folder.Delete(true);
+                            Console.WriteLine("Неиспользуемая папка удалена");
+                        }
+                    }
+                    FileInfo[] files = directory.GetFiles();
+                    foreach (FileInfo file in files)
+                    {
+                        DateTime lastAccess = File.GetLastAccessTime(file.FullName);
+                        if (lastAccess <= lifeSpan)
+                        {
+                            file.Delete();
+                            Console.WriteLine("Неиспользуемый файл удалён");
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
